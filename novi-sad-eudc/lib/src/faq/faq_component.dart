1afa66cd7d394a09848646f7b267ec4559421a64
ralucavacaru <raluca_vacaru@yahoo.com>
2018-06-20 14:46:58 +0100

    Added some content

--- novi-sad-eudc/lib/src/faq/faq_component.dart
+++ novi-sad-eudc/lib/src/faq/faq_component.dart
@@ -1,9 +1,12 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+
 
 @Component(
   selector: 'ns-faq',
   templateUrl: 'faq_component.html',
   styleUrls: const['faq_component.css'],
+  directives: const [ROUTER_DIRECTIVES]
 )
 class FaqComponent {
 }
